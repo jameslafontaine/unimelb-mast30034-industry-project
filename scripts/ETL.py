3530e048e67e99d0173fd132497f162234fa32c7
jameslafontaine <jlafontaine@student.unimelb.edu.au>
2023-09-02 15:40:05 +1000

    Completed basic preprocessing for ETL script

---
+++ scripts/ETL.py
@@ -0,0 +1,9 @@
+###################################################
+## ETL Script for BNPL Industry Project Datasets ##
+##          Author: James La Fontaine            ##
+##           Last Edited: 02/09/2023             ##
+###################################################
+
+
+
+
